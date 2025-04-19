ff4132a27b64b44019fafaae8c9def911d6e05c2
Abdo-Hussien <abdelrahman.hsharaf@gmail.com>
2025-04-19 21:54:49 +0200

    starting up

---
+++ Context.cs
@@ -0,0 +1,116 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+using System.Configuration;
+
+using System.Xml;
+
+namespace Magazine_System
+{
+    internal class Context
+    {
+
+        private readonly string CONNECTION_STR = GetConnectionString("MainDb");
+
+        private OracleConnection Connection { get; set; }
+        private OracleCommand Command { get; set; }
+
+        public Context()
+        {
+            CONNECTION_STR = GetConnectionString("MainDb");
+            Connection = new OracleConnection(CONNECTION_STR);
+        }
+
+        private static string GetConnectionString(string name)
+        {
+            var config = new ExeConfigurationFileMap();
+            config.ExeConfigFilename = "db.config";
+            Configuration cfg = ConfigurationManager.OpenMappedExeConfiguration(config, ConfigurationUserLevel.None);
+            return cfg.ConnectionStrings.ConnectionStrings[name].ConnectionString;
+        }
+
+
+        private void OpenConnection()
+        {
+            try
+            {
+                this.Connection.Open();
+            }
+            catch (OracleException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return;
+            }
+        }
+
+        private void CloseConnection()
+        {
+            if (this.Connection != null && this.Connection.State == ConnectionState.Open)
+            {
+                this.Connection.Close();
+            }
+        }
+
+        private void CreateCommand(string Text, List<OracleParameter> Parameters = null, CommandType Type = CommandType.Text)
+        {
+            this.Command = new OracleCommand(Text, this.Connection);
+            this.Command.CommandType = Type;
+            if (Parameters != null)
+            {
+                foreach (var param in Parameters)
+                {
+                    this.Command.Parameters.Add(param);
+                }
+            }
+        }
+        public void ExecuteCrud(string Text, List<OracleParameter> Parameters = null)
+        {
+            OpenConnection();
+            CreateCommand(Text, Parameters, CommandType.Text);
+            try
+            {
+                //this.Command.ExecuteNonQuery();
+            }
+            catch (OracleException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+        public void ExecuteSelect(string Text, ComboBox Component, List<OracleParameter> Parameters = null)
+        {
+            OpenConnection();
+            CreateCommand(Text, Parameters, CommandType.Text);
+            try
+            {
+                OracleDataReader dr = this.Command.ExecuteReader();
+                while (dr.Read())
+                {
+                    Component.Items.Add(dr[0]);
+                }
+            }
+            catch (OracleException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+
+
+
+
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -8,13 +8,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
 namespace Magazine_System
 {
     public partial class Form1 : Form
     {
+        Context context = new Context();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            context.ExecuteCrud("INSERT INTO magazines (magazine_id, magazine_name, magazine_price) VALUES (:id, :name, :price)",
+                new List<OracleParameter>
+                {
+                    new OracleParameter(":id", 1),
+                    new OracleParameter(":name", "Test Magazine"),
+                    new OracleParameter(":price", 9.99)
+                });
         }
     }
 }
